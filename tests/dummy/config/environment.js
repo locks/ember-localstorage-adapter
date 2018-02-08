62481969d8d37d1d972293f8bf167c7a896ec035
Ricardo Mendes <rokusu@gmail.com>
2018-02-08 01:41:55 +0000

    ember-cli-update --to 2.16

--- tests/dummy/config/environment.js
+++ tests/dummy/config/environment.js
@@ -1,9 +1,10 @@
 /* eslint-env node */
+'use strict';
 
 module.exports = function(environment) {
-  var ENV = {
+  let ENV = {
     modulePrefix: 'dummy',
-    environment: environment,
+    environment,
     rootURL: '/',
     locationType: 'auto',
     EmberENV: {
@@ -43,7 +44,7 @@ module.exports = function(environment) {
   }
 
   if (environment === 'production') {
-
+    // here you can enable a production-specific feature
   }
 
   return ENV;
